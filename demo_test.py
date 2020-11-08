4151b7f8206f782c86e0dc4ce38b9ce3c64a2d13
leezhongguo <leezgfly@hotmail.com>
2020-11-08 14:09:22 +0100

    Delete demo_test.py

--- demo_test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-img_file = '/usr/vision/data/articulate_human_data/bouncing_new/f_0000/Image1_0000.png'
-img = cv2.imread(img_file)
-plt.imshow(img)
-plt.show()
-print("Success!")
