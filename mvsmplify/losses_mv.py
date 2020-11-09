a470a42f7d14c1f55872d00b037dce3432fcecbc
leezhongguo <leezgfly@hotmail.com>
2020-11-09 09:32:50 +0100

    Update losses_mv.py

--- mvsmplify/losses_mv.py
+++ mvsmplify/losses_mv.py
@@ -41,36 +41,6 @@ def body_fitting_loss(body_pose, betas, model_joints, camera_t,camera_center,
                         (joints_conf[int(batch_size/4):int(batch_size/2)] ** 2) * reprojection_error_1[int(batch_size/4):int(batch_size/2)].sum(dim=-1) +\
                         (joints_conf[int(batch_size/2):int(batch_size*3/4)] ** 2) * reprojection_error_1[int(batch_size/2):int(batch_size*3/4)].sum(dim=-1) +\
                         (joints_conf[int(batch_size*3/4):] ** 2) * reprojection_error_1[int(batch_size*3/4):].sum(dim=-1)
-    #rotation = camera_rot[0]
-    #print(camera_rot[1])
-    #input()
-    #camera_rot_mat0 = batch_rodrigues(camera_rot[0])
-    #print(model_joints)
-    #projected_joints_0 = perspective_projection(model_joints[:int(batch_size/4)], rotation, camera_t[:int(batch_size/4)],
-    #                                          focal_length, camera_center[:int(batch_size/4)])
-    #rotation = camera_rot[1]
-    #camera_rot_mat1 = batch_rodrigues(camera_rot[1])
-    #projected_joints_1 = perspective_projection(model_joints[int(batch_size/4):int(batch_size/2)], rotation, camera_t[int(batch_size/4):int(batch_size/2)],
-    #                                          focal_length, camera_center[int(batch_size/4):int(batch_size/2)])
-
-    #rotation = camera_rot[2]
-    #camera_rot_mat2 = batch_rodrigues(camera_rot[2])
-    #projected_joints_2 = perspective_projection(model_joints[int(batch_size/2):int(batch_size*3/4)], rotation, camera_t[int(batch_size/2):int(batch_size*3/4)],
-    #                                          focal_length, camera_center[int(batch_size/2):int(batch_size*3/4)])
-    #rotation = camera_rot[3]
-    #camera_rot_mat3 = batch_rodrigues(camera_rot[3])
-    #projected_joints_3 = perspective_projection(model_joints[int(batch_size*3/4):], rotation, camera_t[int(batch_size*3/4):],
-    #                                          focal_length, camera_center[int(batch_size*3/4):])
-    # Weighted robust reprojection error
-    #reprojection_error_1 = gmof(projected_joints_0 - joints_2d[:int(batch_size/4)], sigma)
-    #reprojection_error_2 = gmof(projected_joints_1 - joints_2d[int(batch_size/4):int(batch_size/2)], sigma)
-    #reprojection_error_3 = gmof(projected_joints_2 - joints_2d[int(batch_size/2):int(batch_size*3/4)], sigma)
-    #reprojection_error_4 = gmof(projected_joints_3 - joints_2d[int(batch_size*3/4):], sigma)
-    
-    #reprojection_loss = (joints_conf[:int(batch_size/4)] ** 2) * reprojection_error_1.sum(dim=-1) +\
-    #                    (joints_conf[int(batch_size/4):int(batch_size/2)] ** 2) * reprojection_error_2.sum(dim=-1) +\
-    #                    (joints_conf[int(batch_size/2):int(batch_size*3/4)] ** 2) * reprojection_error_3.sum(dim=-1) +\
-    #                    (joints_conf[int(batch_size*3/4):] ** 2) * reprojection_error_4.sum(dim=-1)
     # Pose prior loss
     pose_prior_loss = (pose_prior_weight ** 2) * pose_prior(body_pose, betas)
 
@@ -96,7 +66,6 @@ def camera_fitting_loss(model_joints, camera_t, camera_t_est, camera_center, joi
     # Project model joints
     batch_size = model_joints.shape[0]
     rotation = torch.eye(3, device=model_joints.device).unsqueeze(0).expand(batch_size, -1, -1)
-    #print(model_joints.size(),rotation.size(),camera_t.size(),camera_center.size())
     projected_joints = perspective_projection(model_joints, rotation, camera_t,
                                               focal_length, camera_center)
     
@@ -104,9 +73,6 @@ def camera_fitting_loss(model_joints, camera_t, camera_t_est, camera_center, joi
     op_joints_ind = [constants.JOINT_IDS[joint] for joint in op_joints]
     gt_joints = ['Right Hip', 'Left Hip', 'Right Shoulder', 'Left Shoulder']
     gt_joints_ind = [constants.JOINT_IDS[joint] for joint in gt_joints]
-    #print(op_joints_ind,gt_joints_ind)
-    #print(joints_2d[:, gt_joints_ind])
-    #input()
     reprojection_error_op = (joints_2d[:, op_joints_ind] -
                              projected_joints[:, op_joints_ind]) ** 2
     reprojection_error_gt = (joints_2d[:, gt_joints_ind] -
